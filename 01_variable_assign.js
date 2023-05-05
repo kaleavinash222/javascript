03a4a7e540a3eeb0be09d00f754c27b576dc7517
kaleavinash222 <kaleavinash222@gmail.com>
2023-05-05 17:58:02 +0530

    add variable
    add variable

---
+++ 01_variable_assign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName="Avinash Kale";
+console.log(myFullName);
+
+var myFullName="Avinash Rangnath Kale";
+console.log(myFullName);
+
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var firstName="firstName : Avinash";
+var lastName="lastName : Kale";
+var age="age : 25";
+var address="address : beed";
+var pincode="pincode : 414202";
+var gender="gender : Male";
+var isMarried="isMarried : UnMarried";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pincode);
+console.log(gender);
+console.log(isMarried);
