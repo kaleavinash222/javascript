0c9245c17eb5e88a7d63242a9e02cfc69936923b
kaleavinash222 <kaleavinash222@gmail.com>
2023-05-08 06:58:36 +0530

    added variabals
    added variabals

---
+++ .vscode/03_typeOf.js
@@ -0,0 +1,17 @@
+var bankName;
+console.log("Bank Name   : ",bankName,"                DataType : ",typeof bankName);
+var bankName="SBI bank";
+console.log("Bank Name   : ",bankName,"                 DataType : ",typeof bankName);
+
+var marks='90%';
+console.log("Marks       : ",marks,"                      DataType : ",typeof marks);
+
+var isWorking;
+console.log("isWorking   : ",isWorking,"                Datatype : ",typeof isWorking);
+var isWorking=true;
+console.log("Is Working  : ",isWorking,"                     DataType : ",typeof isWorking);
+
+var totalCount="Hundred and seven";
+console.log("Total Count : ",totalCount,"        DataType : ",typeof totalCount);
+var totalCount=107;
+console.log("Total Count : ",totalCount,"                      DataType : ",typeof totalCount);
---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,32 @@
+var rollNo=11;
+var mobileNumber=8888777555;
+var age=25;
+
+var sname="Avinash";
+var address="beed";
+var collageName="Balbhim collage";
+
+var isMarried= false;
+var isBoy=true;
+var isGirl=false;
+
+console.log("Roll No :",rollNo,"                 Type : ",typeof rollNo);
+console.log("Mobile Number : ",mobileNumber,"  Type : ",typeof mobileNumber);
+console.log("Age : ",age,"                    Type : ",typeof age);
+
+console.log("_______________________________________________________");
+
+console.log("Name : ",sname,"              Type : ",typeof sname);
+console.log("Address : ",address,"              Type : ",typeof address);
+console.log("Collage : ",collageName,"   Type : ",typeof collageName);
+
+console.log("_______________________________________________________");
+
+console.log("IsMarried : ",isMarried,     "    Type : ",typeof isMarried);
+console.log("IsBoy : ",isBoy,             "         Type : ",typeof isBoy);
+console.log("IdGirl : ",isGirl,           "       Type : ",typeof isGirl);
+
+console.log("_______________________________________________________");
+
+console.log("Null = null means an empty or non-existent value.Null is assigned,and explicitly means nothing.");
+console.log("Undefiend = Undefiend means a variable is declared but the value of that variable has not yet beed defiend.");
