899038724e9e7cbd0d81741069dfd89a4e088baa
kaleavinash222 <kaleavinash222@gmail.com>
2023-05-26 19:25:49 +0530

    whileloop
    whileloop

---
+++ 08_whileAssign.js
@@ -0,0 +1,60 @@
+console.log(`1.print number from 5 to 15 by increment 1.`);
+let i=5;
+while (i<=15){
+    console.log(i);
+    i=i+1;   
+}
+
+console.log(`2.print number from 50 to 40 by dncrement 1.`);
+let j=50;
+while (j>=40) {
+    console.log(j);
+    j=j-1;
+    
+}
+
+console.log(`3.find first 15 odd number.`);
+let k=0;
+  while (k<=30) {
+    if (k%2!=0) {
+        console.log(k);
+    }
+    k=k+1;
+}
+
+console.log(`4.find first 10 even number.`);
+a=0;
+while (a<=19) {
+    if (a%2==0) {
+        console.log(a);
+        
+    }
+    a=a+1;
+}
+
+
+console.log(`5.print table of 5 like ..5,10,15.....50`);
+let b=5;
+while (b<=50) {
+        console.log(b);
+        b=b+5;
+    }
+    
+
+    console.log(`6.print table of 10 like ..10,20,30.....100`);
+    let c=10;
+    while (c<=100) {
+        console.log(c);
+        c=c+10;
+        
+    }
+    console.log(`7.print table  revers order of 10 like ..100,90,80.....10`);
+    let d=100;
+    while (d>=10) {
+        console.log(d);
+        d=d-10;
+        
+    }
+
+
+
